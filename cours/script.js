4f226b9a62d5a2083041281615512997afe7b39f
charnelledev <kengnemaiva54@gmail.com>
2025-02-03 15:21:43 +0100

    tp-blague

--- cours/script.js
+++ cours/script.js
@@ -16,3 +16,37 @@ xhr.send();
 
 //gerer les erreur du resaux
 xhr.onerror = ()=>{};
+
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('Get', 'https://api.blablagues.net/?rub=blagues', true);
+
+
+xhr.onload = function(){
+    if (xhr.status>=200 && xhr.status<300){
+
+        console.log(xhr.responseText);
+    }else{
+        console.error("Error",responseText);
+    }
+
+}
+
+
+xhr.onerror = ()=>{
+    // console.log('Erreur de resau');
+    
+}
+xhr.send()
+
+
+
+
+
+
+fetch("https://api.blablagues.net/?rub=blagues").then((resp)=>resp.json())
+.then((blague)=>{
+    console.log(blague.data);
+    
+})
